e3e688de547d0992aec9443dd36d7f39749710b3
skorokhodtatiana <tanjakharza@mail.ru>
2021-12-06 08:44:32 +0300

    improve code

--- 1-task-superheros/superhero.js
+++ 1-task-superheros/superhero.js
@@ -1,56 +1,131 @@
 class Superhero {
-    constructor(character, universe, name, career, friends, image) {
-
-        this.character = character;
-        this.universe = universe;
-        this.name = name;
-        this.career = career;
-        this.friends = friends;
-        this.image = image;
-    }
+  constructor(character, universe, name, career, friends, image) {
+    this.character = character;
+    this.universe = universe;
+    this.name = name;
+    this.career = career;
+    this.friends = friends;
+    this.image = image;
+  }
 }
 
 let superheroes = [];
 
-let batman = new Superhero("Бэтмэн", "DC-Comics", " Брюс Уэйн", "борец с преступностью, филантроп, миллиардер", " Робин, Бэтгерл");
+let batman = new Superhero(
+  "Бэтмэн",
+  "DC-Comics",
+  " Брюс Уэйн",
+  "борец с преступностью, филантроп, миллиардер",
+  " Робин, Бэтгерл"
+);
 
-let superman = new Superhero("Супермэн", "DC-Comics", "Кларк Кент", "борец за справедливость", "собака Крипто", "images/superman.jpg");
+let superman = new Superhero(
+  "Супермэн",
+  "DC-Comics",
+  "Кларк Кент",
+  "борец за справедливость",
+  "собака Крипто",
+  "images/superman.jpg"
+);
 
-let ironman = new Superhero("Железный человек", "Marvel Comics", "Тони Старк", "гений, миллиардер, плейбой, филантроп", "Мстители");
+let ironman = new Superhero(
+  "Железный человек",
+  "Marvel Comics",
+  "Тони Старк",
+  "гений, миллиардер, плейбой, филантроп",
+  "Мстители"
+);
 
-let spiderman = new Superhero("Спайдер-мен/Человек-паук", "Marvel Comics", "Питер Паркер", "борец за справедливость, студент, фотограф", "Мстители, Фантастическая четверка, Люди Икс");
+let spiderman = new Superhero(
+  "Спайдер-мен/Человек-паук",
+  "Marvel Comics",
+  "Питер Паркер",
+  "борец за справедливость, студент, фотограф",
+  "Мстители, Фантастическая четверка, Люди Икс"
+);
 
-let captainamerica = new Superhero("Капитан Америка", "Marvel Comics", "Стивен Роджерс", "супер-солдат", "Мстители");
+let captainamerica = new Superhero(
+  "Капитан Америка",
+  "Marvel Comics",
+  "Стивен Роджерс",
+  "супер-солдат",
+  "Мстители"
+);
 
-let thor = new Superhero("Тор", "Marvel Comics", "нет; полное имя — Тор Одинсон", "борец за справедливость, скандинавский бог", "Мстители");
+let thor = new Superhero(
+  "Тор",
+  "Marvel Comics",
+  "нет; полное имя — Тор Одинсон",
+  "борец за справедливость, скандинавский бог",
+  "Мстители"
+);
 
-let hulk = new Superhero("Халк", "Marvel Comics", "Брюс Беннер", "супергерой, борец за справедливость, ученый-биохимик", "Мстители", "images/hulk.jpg");
+let hulk = new Superhero(
+  "Халк",
+  "Marvel Comics",
+  "Брюс Беннер",
+  "супергерой, борец за справедливость, ученый-биохимик",
+  "Мстители",
+  "images/hulk.jpg"
+);
 
-let wonderwoman = new Superhero("Чудо-женщина", "DC Comics", "Диана Принс", "супергероиня, секретарь-референт", "Лига Справедливости, Бэтмен, Супермен");
+let wonderwoman = new Superhero(
+  "Чудо-женщина",
+  "DC Comics",
+  "Диана Принс",
+  "супергероиня, секретарь-референт",
+  "Лига Справедливости, Бэтмен, Супермен"
+);
 
-let blackwidow = new Superhero("Черная вдова", "Marvel Comics", "Наташа Романофф", "супергероиня, шпионка", "Мстители");
+let blackwidow = new Superhero(
+  "Черная вдова",
+  "Marvel Comics",
+  "Наташа Романофф",
+  "супергероиня, шпионка",
+  "Мстители"
+);
 
-let deadpool = new Superhero("Дэдпул", "Marvel Comics", "Уэйд Уинстон Уилсон", "антигерой, наемник", "частично Мстители, Человек-паук, Росомаха");
+let deadpool = new Superhero(
+  "Дэдпул",
+  "Marvel Comics",
+  "Уэйд Уинстон Уилсон",
+  "антигерой, наемник",
+  "частично Мстители, Человек-паук, Росомаха"
+);
 
-superheroes.push(batman, superman, ironman, spiderman, captainamerica, thor, hulk, wonderwoman, blackwidow, deadpool);
+superheroes.push(
+  batman,
+  superman,
+  ironman,
+  spiderman,
+  captainamerica,
+  thor,
+  hulk,
+  wonderwoman,
+  blackwidow,
+  deadpool
+);
 
 let serializedHeroes = JSON.stringify(superheroes);
 
-document.addEventListener('DOMContentLoaded', function (ev) {
-    let parsedHeroes = JSON.parse(serializedHeroes);
+document.addEventListener("DOMContentLoaded", function (ev) {
+  let parsedHeroes = JSON.parse(serializedHeroes);
 
-    let listHeroes = "";
+  let listHeroes = "";
 
-    for (let i = 0; i < parsedHeroes.length; i++) {
-        let hero = parsedHeroes[i];
-        listHeroes += `<div class='cards'>
+  for (let i = 0; i < parsedHeroes.length; i++) {
+    let hero = parsedHeroes[i];
+    listHeroes +=
+      `<div class='cards'>
             <h2 class="header">${hero.character}</h2>
             <div>Вселенная: ${hero.universe}</div>
             <div>Альтер эго: ${hero.name}</div>
             <div>Род деятельности: ${hero.career}</div>
             <div>Друзья: ${hero.friends}</div>` +
-            ((hero.image == undefined) ? `` : `<img class="imgHero" src="${hero.image}"/>`) +
-            `<form action="#" id="formUser" class="rating-wrapper">
+      (hero.image == undefined
+        ? ``
+        : `<img class="imgHero" src="${hero.image}"/>`) +
+      `<form action="#" id="formUser" class="rating-wrapper">
             <div class="rating-items">
 
             <input type="radio" id="rating5-${i}" class="rating-item" name="rating" onclick="getRating(this)" value="5"/>
@@ -78,33 +153,33 @@ document.addEventListener('DOMContentLoaded', function (ev) {
             <span></span>
         </div>
         </div>`;
-    }
-    document.getElementById('herosContainer').innerHTML = listHeroes;
-    return parsedHeroes;
-})
+  }
+  document.getElementById("herosContainer").innerHTML = listHeroes;
+  return parsedHeroes;
+});
 
 function getRating(elem) {
-    let ratingValue = elem.value;
+  let ratingValue = elem.value;
 
-    let parsedHeroes = JSON.parse(serializedHeroes);
-    let numHero = elem.id.slice(-1);
-    let nameHeros = parsedHeroes[numHero].character;
+  let parsedHeroes = JSON.parse(serializedHeroes);
+  let numHero = elem.id.slice(-1);
+  let nameHeros = parsedHeroes[numHero].character;
 
-    let arrRat = [];
-    let getValue = localStorage.getItem(nameHeros);
-    if (getValue) {
-        arrRat.push(getValue);
-        arrRat.push(ratingValue);
-        localStorage.setItem(nameHeros, arrRat);
-    } else {
-        if (checkEmpty(nameHeros, ratingValue)) {
-            localStorage.setItem(nameHeros, ratingValue);
-        }
+  let arrRat = [];
+  let getValue = localStorage.getItem(nameHeros);
+  if (getValue) {
+    arrRat.push(getValue);
+    arrRat.push(ratingValue);
+    localStorage.setItem(nameHeros, arrRat);
+  } else {
+    if (checkEmpty(nameHeros, ratingValue)) {
+      localStorage.setItem(nameHeros, ratingValue);
     }
-    console.log(`${nameHeros}: ${arrRat.toString()}`)
-    return nameHeros, ratingValue;
+  }
+  console.log(`${nameHeros}: ${arrRat.toString()}`);
+  return nameHeros, ratingValue;
 }
 
 function checkEmpty(nameHeros, ratingValue) {
-    return (nameHeros != "" && ratingValue != "") ? true : false;
+  return nameHeros != "" && ratingValue != "" ? true : false;
 }
